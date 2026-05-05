d591f04734cd1ab65b4af7ba287e0d3ee9db3523
Sukriti Dubey <sukritidubey@Sukritis-MacBook-Air.local>
2026-05-05 00:40:15 -0700

    adding vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Playfair_Display, Lato } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const playfair = Playfair_Display({
@@ -34,6 +35,7 @@ export default function RootLayout({
         className={`${playfair.variable} ${lato.variable} font-lato antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
